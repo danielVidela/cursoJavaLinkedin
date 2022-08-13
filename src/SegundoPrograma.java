edba3df7690697c0a88ec16bf5d111ef0eedcb0c
Daniel Videla <daniel.videla.developer@gmail.com>
2022-08-13 10:31:46 -0300

    segundo programa

---
+++ src/SegundoPrograma.java
@@ -0,0 +1,12 @@
+public class SegundoPrograma {
+    public static void main(String[] args) {
+        int variableUno = 5;
+        int variableDos = 10;
+        if  (variableUno > variableDos){
+            System.out.println("variableUno es mayor que variableDos");
+
+        }else{
+            System.out.println("variableDos es mayor que variableUno");
+        }
+    }
+}
